767ae294e2e7e8b7ff22983b6579ccabc56ee168
arshak91 <arshakgasparyan091@gmail.com>
2025-07-13 17:48:38 +0400

    changes

--- src/controllers/inVoice.js
+++ src/controllers/inVoice.js
@@ -1,11 +1,13 @@
 import { Users } from "../models/users.js";
 import { inVoice } from "../models/invoice.js";
+import { Orders } from "../models/orders.js";
 
 
 export const createInVoice = async (req, res) => {
   const { orderId, count, price } = req.body;
   const userId = req.user.id
   const user = await Users.findById(userId);
+  const order = await Orders.findById(orderId)
   const totalPrice = count * price;
   if(totalPrice > user.wallet) {
     return res.status(402).json({ message: 'no enough money' });
@@ -15,6 +17,13 @@ export const createInVoice = async (req, res) => {
   }, {
     wallet: user.wallet - totalPrice
   },{new: true});
+  const orderData = await Orders.findOneAndUpdate({
+    _id: orderId
+  }, {
+    count: order.count - count
+  }, {new: true})
+  console.log(orderData);
+  
 
   console.log(userData);
   const inVoiceRes = await inVoice.create({
